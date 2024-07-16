e2fb51c8d28ecc1fe28350bd58bc1350f44ee5b6
hieubgjm <hieubgjm@gmail.com>
2024-07-16 20:00:07 +0700

    Done DB Code First

---
+++ MyShop.Model/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using MyShop.Model.Abstract;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyShop.Model.Models
+{
+    [Table("Category")]
+    public class Category : Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string? Alias { get; set; }
+        public string? Description { get; set; }
+        public int? ParentID { get; set; }
+        public int? DisplayOrder { get; set; }
+        public string? Image { get; set; }
+        public bool? HotFlad { get; set; }
+    }
+}
---
+++ MyShop.Model/Models/MenuGroup.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyShop.Model.Models
+{
+    [Table("MenuGroup")]
+    public class MenuGroup
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public virtual IEnumerable<Menu> Menus { get; set; }
+    }
+}
